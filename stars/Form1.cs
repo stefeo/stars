3036c66e316d04268a1e56e4e9e9ebe0856b1551
stefeo <stefeo9@gmail.com>
2015-11-23 14:26:09 -0500

    Method created for starfield

--- stars/Form1.cs
+++ stars/Form1.cs
@@ -16,5 +16,29 @@ namespace stars
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StarField(100);
+        }
+        private void StarField(int x)
+        {
+            Graphics formGraphics = this.CreateGraphics();
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+            int xcoord, ycoord, size, rcolor, gcolor, bcolor, w;
+            Random randNum = new Random();
+
+            for (w = 0; w <= x; w++)
+            {
+                rcolor = randNum.Next(1, 255);
+                gcolor = randNum.Next(1, 255);
+                bcolor = randNum.Next(1, 255);
+                drawBrush.Color = Color.FromArgb(rcolor, gcolor, bcolor);
+                xcoord = randNum.Next(1, 300);
+                ycoord = randNum.Next(1, 300);
+                size = randNum.Next(1, 10);
+                formGraphics.FillRectangle(drawBrush, xcoord, ycoord, size, size);
+            }
+        }
     }
 }
